2779dc7e5a41d6ac92634aa578b2f4be52c8884f
Soheil Karami <sohil_ww@ymail.com>
2016-08-04 00:26:46 +0430

    Add Upload Script
    Ajax Upload Script Is Ajax File Uploader With out Jquery And other
    Plugin

---
+++ UploadScript.js
@@ -0,0 +1,31 @@
+﻿/// Created By Mr Karami   
+// GitHub :https://github.com/sohilww/AjaxFileUploder
+var progressbar;
+function uploadFile(inputElement, fileName, url, hiddenInput, progressbars) {
+    var file = document.getElementById(inputElement).files[0];
+    progressbar = progressbars;
+    var formData = new FormData();
+    formData.append(fileName, file);
+    var xhr = new XMLHttpRequest();
+    xhr.upload.addEventListener("progress", progressHandler, false);
+    xhr.upload.addEventListener("load", completeHandler, false);
+    xhr.open("Post", url);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var rsult = JSON.parse(xhr.responseText);
+            if (rsult.Status) {
+                $(hiddenInput).val(rsult.Message);
+            }
+
+        }
+    };
+    xhr.send(formData);
+}
+function progressHandler(event) {
+    var percent = ((event.loaded / event.total) * 100) + "%";
+    $(progressbar).show();
+    $(".progress-bar", $(progressbar)).width(percent);
+}
+function completeHandler() {
+    // myApp.hidePleaseWait(); //hide dialog
+}
